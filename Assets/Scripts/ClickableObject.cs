4584323e296a88839ea9a531408f00ed5ddd6f08
Ameize <ameize4@gmail.com>
2019-08-03 20:38:49 +0300

    Append main menu

---
+++ Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickableObject : MonoBehaviour
+{
+    
+    public void SetColorBlack()
+    {
+        GetComponent<Text>().color = Color.black;
+    }
+    
+    public void SetOldColor()
+    {
+        GetComponent<Text>().color = new Color(222, 178, 0);
+    }
+}
+
