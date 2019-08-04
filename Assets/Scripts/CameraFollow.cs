04107d526a7a6886ad85006bffaff88e3c108c29
Ameize <ameize4@gmail.com>
2019-08-04 11:04:07 +0300

    Add base combat

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float offsetX;
+
+    void Update()
+    {
+        transform.position =
+            new Vector3(player.position.x + offsetX, transform.position.y, transform.position.z); 
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed;
+
+    private float moveInput;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput*speed, rb.velocity.y);
+    }
+}
